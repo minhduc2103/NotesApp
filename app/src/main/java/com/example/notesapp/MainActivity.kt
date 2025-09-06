bfa5dd5a91b19e4e8e9b0dcf095cf3f2a5278bed
Minh Duc <tranminhducbs123@gmail.com>
2025-09-06 16:09:19 +0700

    First commit

---
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.notesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.example.notesapp.database.NoteDatabase
+import com.example.notesapp.repository.NoteRepository
+import com.example.notesapp.ui.theme.NotesAppTheme
+import com.example.notesapp.viewmodel.NoteViewModel
+import com.example.notesapp.viewmodel.NoteViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+}
+
---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.androidx.navigation.safe.args.gradle.plugin)
+    }
+}
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+}
